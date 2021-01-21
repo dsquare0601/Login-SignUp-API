f27a1d3e28dd566a7b8c86eebe57bd4c62b50699
Dhaval Darji <62747051+dsquare0601@users.noreply.github.com>
2021-01-21 19:39:36 +0530

    Update dbConfig.js

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,6 +1,4 @@
 module.exports = {
     url : "mongodb+srv://dbDsquare:dbDsquareMongo@dsquare.kjrng.gcp.mongodb.net/test-api",
-    APIKey : "ee1186c852e377324ccd516cbda72db8-28d78af2-08f6d8d2",
-    DomainName: "sandboxc53b83acbb0745c180bc5be56e792a97.mailgun.org",
     LocalURL : "http://localhost:3000"
 }
