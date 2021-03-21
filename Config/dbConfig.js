c0c767ab02b53f808a71170182241d82462ff849
Dhaval Darji <62747051+dsquare0601@users.noreply.github.com>
2021-03-21 10:26:15 +0530

    Update dbConfig.js

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url : "mongodb+srv://dbDsquare:dbDsquareMongo@dsquare.kjrng.gcp.mongodb.net/test-api",
+    url : "mongourl/test-api",
     LocalURL : "http://localhost:3000"
 }
